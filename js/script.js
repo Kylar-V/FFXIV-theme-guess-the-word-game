7c2108972b70b41189a19db37227b35ab73e1944
Kai <kylar_valentine@hotmail.co.uk>
2024-05-17 16:38:39 +0200

    changed background image and font color

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const playAgainButton = document.querySelector(".btn-play-again");
 
 let word = "fire";
 let guessedLetters = [];
-let remainingGuesses = 12;
+let remainingGuesses = 10; // Changed to 10
 
 const classesWords = [
   "Paladin",
@@ -179,18 +179,6 @@ const gameMechanics = [
   "Triple Triad",
 ];
 
-const placeholder = function (word) {
-  const placeholderLetters = [];
-  for (const letter of word) {
-    if (letter === " ") {
-      placeholderLetters.push(" ");
-    } else {
-      placeholderLetters.push("●");
-    }
-  }
-  wordInProgress.innerText = placeholderLetters.join("");
-};
-
 const getWord = function () {
   const categories = [
     classesWords,
@@ -208,7 +196,62 @@ const getWord = function () {
   placeholder(word);
 };
 
-// Call the getWord function to choose a random word
+const placeholder = function (word) {
+  const placeholderLetters = [];
+  for (const letter of word) {
+    if (letter === " ") {
+      placeholderLetters.push(" ");
+    } else {
+      placeholderLetters.push("●");
+    }
+  }
+  wordInProgress.innerText = placeholderLetters.join("");
+};
+
+const startOver = function (hasWon) {
+  guessLetterButton.classList.add("hide");
+  remainingGuessesElement.classList.add("hide");
+  guessedLettersElement.classList.add("hide");
+  playAgainButton.classList.remove("hide");
+
+  // Disable the input field
+  letterInput.disabled = true;
+
+  if (hasWon) {
+    message.classList.add("win");
+    message.innerHTML = `<p class="highlight">Congratulations Adventurer! You guessed the word correctly!</p>`;
+  } else {
+    message.innerHTML = `You have failed Adventurer! The word was <span class="highlight">${word}</span>.`;
+  }
+
+  // Re-enable the input field and reset the game state when the player clicks the "Play Again" button
+  playAgainButton.addEventListener(
+    "click",
+    function () {
+      guessedLetters = [];
+      remainingGuesses = 10; // Reset to 10
+      remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
+      guessedLettersElement.innerHTML = "";
+      message.innerText = "";
+      getWord(); // Call getWord to select a new word
+      placeholder(word);
+
+      // Re-enable the input field
+      letterInput.disabled = false;
+
+      // Hide the "Play Again" button
+      playAgainButton.classList.add("hide");
+
+      // Show the guess button and remaining guesses element
+      guessLetterButton.classList.remove("hide");
+      remainingGuessesElement.classList.remove("hide");
+      guessedLettersElement.classList.remove("hide");
+    },
+    { once: true }
+  ); // Use the "once" option to ensure the event listener is only added once
+};
+
+// Initial setup
 getWord();
 
 guessLetterButton.addEventListener("click", function (e) {
@@ -317,42 +360,4 @@ const checkIfWin = function (revealWord) {
     message.innerHTML = `<p class="highlight">Congratulations Adventurer! You guessed the word correctly!</p>`;
     startOver(true); // Pass true to indicate the player won
   }
-};
-
-const startOver = function (hasWon) {
-  guessLetterButton.classList.add("hide");
-  remainingGuessesElement.classList.add("hide");
-  guessedLettersElement.classList.add("hide");
-  playAgainButton.classList.remove("hide");
-
-  // Disable the input field
-  letterInput.disabled = true;
-
-  if (hasWon) {
-    message.classList.add("win");
-    message.innerHTML = `<p class="highlight">Congratulations Adventurer! You guessed the word correctly!</p>`;
-  } else {
-    message.innerHTML = `You have failed Adventurer! The word was <span class="highlight">${word}</span>.`;
-  }
-
-  // Re-enable the input field when the player clicks the "Play Again" button
-  playAgainButton.addEventListener("click", function () {
-    guessedLetters = [];
-    remainingGuesses = 12;
-    remainingGuessesSpan.innerText = `${remainingGuesses} guesses`;
-    guessedLettersElement.innerHTML = "";
-    message.innerText = "";
-    placeholder(word);
-
-    // Re-enable the input field
-    letterInput.disabled = false;
-
-    // Hide the "Play Again" button
-    playAgainButton.classList.add("hide");
-
-    // Show the guess button and remaining guesses element
-    guessLetterButton.classList.remove("hide");
-    remainingGuessesElement.classList.remove("hide");
-    guessedLettersElement.classList.remove("hide");
-  });
 };
