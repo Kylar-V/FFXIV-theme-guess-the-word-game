9d59315a6f37e2d5515ee26e8a9db027898e473a
Kai <kylar_valentine@hotmail.co.uk>
2024-05-28 12:52:50 +0200

    gave the hint button a new function, tweaked responsiveness and midified the css

--- js/script.js
+++ js/script.js
@@ -2,482 +2,377 @@ const wordData = {
   classesWords: [
     {
       word: "Paladin",
-      hint: "Masters of sword and shield, defenders of justice.",
     },
     {
       word: "Warrior",
-      hint: "Fierce warriors wielding massive axes, embodying strength and resilience.",
     },
     {
       word: "Dark Knight",
-      hint: "Wielders of darkness, clad in imposing armor, seeking redemption.",
     },
     {
       word: "White Mage",
-      hint: "Healers who harness the power of light, bringing life and protection.",
     },
     {
       word: "Scholar",
-      hint: "Tactical healers using faeries to support allies.",
     },
     {
       word: "Astrologian",
-      hint: "Mystics drawing power from the stars to heal and protect.",
     },
     {
       word: "Monk",
-      hint: "Martial artists with powerful hand-to-hand combat skills.",
     },
     {
       word: "Dragoon",
-      hint: "Lancers with the ability to leap high and deal devastating attacks.",
     },
-    { word: "Ninja", hint: "Stealthy assassins utilizing ninjutsu and speed." },
+    { word: "Ninja" },
     {
       word: "Samurai",
-      hint: "Masters of the katana with swift and deadly strikes.",
     },
     {
       word: "Bard",
-      hint: "Archers who support allies with songs and melodies.",
     },
     {
       word: "Machinist",
-      hint: "Engineers wielding firearms and automated turrets.",
     },
     {
       word: "Black Mage",
-      hint: "Spellcasters wielding powerful destructive magic.",
     },
-    { word: "Summoner", hint: "Mages who summon entities to aid in battle." },
+    { word: "Summoner" },
     {
       word: "Red Mage",
-      hint: "Hybrid spellcasters balancing white and black magic.",
     },
-    { word: "Blue Mage", hint: "Mimics enemy abilities to use in battle." },
+    { word: "Blue Mage" },
     {
       word: "Gunbreaker",
-      hint: "Fighters using gunblades, combining swordplay and gunfire.",
     },
     {
       word: "Dancer",
-      hint: "Performers who use dances to deal damage and support allies.",
     },
     {
       word: "Marauder",
-      hint: "Fearsome axe-wielders who crush their foes with brute strength.",
     },
     {
       word: "Gladiator",
-      hint: "Versatile fighters skilled with swords and shields.",
     },
     {
       word: "Conjurer",
-      hint: "Healers who call upon the elements to support their allies.",
     },
     {
       word: "Arcanist",
-      hint: "Magical tacticians who summon familiars to assist in combat.",
     },
     {
       word: "Pugilist",
-      hint: "Hand-to-hand combat specialists who excel in close-quarters.",
     },
     {
       word: "Lancer",
-      hint: "Polearm-wielders known for their offensive prowess and agility.",
     },
     {
       word: "Rogue",
-      hint: "Agile fighters who specialize in stealth and dexterity.",
     },
     {
       word: "Archer",
-      hint: "Ranged attackers who excel in precision and strategy.",
     },
     {
       word: "Thaumaturge",
-      hint: "Spellcasters who wield the powers of fire, ice, and lightning.",
     },
     {
       word: "Alchemist",
-      hint: "Craftsmen who create potions and perform transmutations.",
     },
     {
       word: "Carpenter",
-      hint: "Artisans who create wooden weapons and furniture.",
     },
-    { word: "Blacksmith", hint: "Smiths who forge metal weapons and armor." },
+    { word: "Blacksmith" },
     {
       word: "Armorer",
-      hint: "Craftsmen who create heavy armor and protective gear.",
     },
-    { word: "Goldsmith", hint: "Artisans who craft jewelry and accessories." },
+    { word: "Goldsmith" },
     {
       word: "Leatherworker",
-      hint: "Crafters specializing in leather armor and goods.",
     },
-    { word: "Weaver", hint: "Tailors who create cloth armor and garments." },
+    { word: "Weaver" },
     {
       word: "Culinarian",
-      hint: "Chefs who prepare meals to boost allies' abilities.",
     },
     {
       word: "Miner",
-      hint: "Gatherers who extract minerals and ores from the earth.",
     },
-    { word: "Botanist", hint: "Harvesters who gather plants and herbs." },
-    { word: "Fisher", hint: "Anglers who catch fish and other aquatic life." },
+    { word: "Botanist" },
+    { word: "Fisher" },
   ],
   mainCharacterFirstNames: [
-    { word: "Y'shtola", hint: "A member of the Scions of the Seventh Dawn." },
-    { word: "Thancred", hint: "A gunbreaker and Scion of the Seventh Dawn." },
+    { word: "Y'shtola" },
+    { word: "Thancred" },
     {
       word: "Urianger",
-      hint: "A keeper of ancient knowledge within the Scions of the Seventh Dawn.",
     },
     {
       word: "Alphinaud",
-      hint: "A young and ambitious member of the Scions of the Seventh Dawn.",
     },
     {
       word: "Alisaie",
-      hint: "Alphinaud's twin sister and fellow Scion of the Seventh Dawn.",
     },
     {
       word: "Minfilia",
-      hint: "A former leader of the Path of the Twelve and Scion of the Seventh Dawn.",
     },
     {
       word: "Tataru",
-      hint: "A talented receptionist and member of the Scions of the Seventh Dawn.",
     },
     {
       word: "Yda",
-      hint: "A fiery member of the Path of the Twelve and Scion of the Seventh Dawn.",
     },
     {
       word: "Papalymo",
-      hint: "A wise and powerful Lalafell and Scion of the Seventh Dawn.",
     },
-    { word: "Estinien", hint: "A dragoon and Azure Dragoon of Ishgard." },
-    { word: "Aymeric", hint: "A noble and charismatic leader of Ishgard." },
-    { word: "Haurchefant", hint: "A loyal and selfless knight of Ishgard." },
-    { word: "Yugiri", hint: "A mysterious shinobi from the Far East." },
-    { word: "Lyse", hint: "A passionate leader of the Ala Mhigan Resistance." },
+    { word: "Estinien" },
+    { word: "Aymeric" },
+    { word: "Haurchefant" },
+    { word: "Yugiri" },
+
+    { word: "Lyse" },
+
     {
       word: "Raubahn",
-      hint: "A legendary gladiator and leader of the Immortal Flames.",
     },
+
     {
       word: "Kan-E-Senna",
-      hint: "A wise and respected elder of the Conjurers' Guild in Gridania.",
     },
-    { word: "Merlwyb", hint: "A fierce pirate and leader of Limsa Lominsa." },
+
+    { word: "Merlwyb" },
+
     {
       word: "Nanamo",
-      hint: "The sultana of Ul'dah known for her compassion and wisdom.",
     },
   ],
   mainLocations: [
     {
       word: "Limsa Lominsa",
-      hint: "A maritime city-state known for its bustling ports and maritime culture.",
     },
     {
       word: "Ul'dah",
-      hint: "A desert city-state renowned for its wealth, trade, and bustling markets.",
     },
     {
       word: "Gridania",
-      hint: "A forested city-state nestled deep within the Black Shroud, known for its connection to nature and the elementals.",
     },
     {
       word: "Ishgard",
-      hint: "A fortified city-state situated in the snow-capped mountains of Coerthas, known for its eternal conflict with dragons.",
     },
     {
       word: "Kugane",
-      hint: "A vibrant port city in the Far East, serving as a melting pot of cultures and trade.",
     },
     {
       word: "Idyllshire",
-      hint: "A bustling hub of industry and innovation, built upon the ruins of Sharlayan.",
     },
     {
       word: "Rhalgr's Reach",
-      hint: "A fortress settlement in Gyr Abania, serving as the headquarters of the Ala Mhigan Resistance.",
     },
     {
       word: "The Crystarium",
-      hint: "A beacon of hope in the First, built by the people of Norvrandt to shelter those displaced by the Flood of Light.",
     },
     {
       word: "Eulmore",
-      hint: "A decadent city ruled by the rich and powerful, located in the region of Kholusia.",
     },
     {
       word: "The Rising Stones",
-      hint: "A hidden outpost of the Scions of the Seventh Dawn, located in Mor Dhona.",
     },
     {
       word: "The Waking Sands",
-      hint: "The original headquarters of the Scions of the Seventh Dawn, located in Vesper Bay.",
     },
     {
       word: "The Firmament",
-      hint: "A district in Ishgard dedicated to the reconstruction efforts following the end of the Dragonsong War.",
     },
     {
       word: "The Gold Saucer",
-      hint: "An amusement park and entertainment center located near Ul'dah, offering various attractions and minigames.",
     },
     {
       word: "The Ruby Sea",
-      hint: "A vast expanse of water located between Othard and Hingashi, known for its lush islands and underwater settlements.",
     },
     {
       word: "Yanxia",
-      hint: "A war-torn region in Othard, marked by its rice fields, rivers, and the presence of the Garlean Empire.",
     },
   ],
+
   primalWords: [
     {
       word: "Ifrit",
-      hint: "A primal associated with fire, known for his blazing inferno attacks.",
     },
     {
       word: "Titan",
-      hint: "A primal associated with earth, revered as the Lord of Crags.",
     },
     {
       word: "Garuda",
-      hint: "A primal associated with wind, known for her swift and merciless attacks.",
     },
     {
       word: "Leviathan",
-      hint: "A primal associated with water, feared for his ability to summon devastating tidal waves.",
     },
     {
       word: "Ramuh",
-      hint: "A primal associated with lightning, known as the Lord of Levin.",
     },
     {
       word: "Shiva",
-      hint: "A primal associated with ice, revered as the Lady of Frost.",
     },
     {
       word: "Ravana",
-      hint: "A primal associated with insects, known for his formidable martial prowess.",
     },
     {
       word: "Bismarck",
-      hint: "A primal associated with wind, revered as the Lord of the Mists.",
     },
     {
       word: "Alexander",
-      hint: "A primal associated with machinery, known for his mechanical marvels and time-manipulating abilities.",
     },
     {
       word: "Odin",
-      hint: "A primal associated with darkness, known as the Dark Divinity.",
     },
     {
       word: "Ultima Weapon",
-      hint: "A primal associated with destruction, wielding the power of Allagan technology.",
     },
     {
       word: "Sephirot",
-      hint: "A primal associated with earth, revered as the Fiend.",
     },
     {
       word: "Nidhogg",
-      hint: "A primal associated with dragons, known as the Wyrmking.",
     },
     {
       word: "Sophia",
-      hint: "A primal associated with wisdom, revered as the Goddess.",
     },
     {
       word: "Susano",
-      hint: "A primal associated with storms, known as the Lord of Revel.",
     },
     {
       word: "Byakko",
-      hint: "A primal associated with lightning, known as the White Tiger.",
     },
     {
       word: "Tsukuyomi",
-      hint: "A primal associated with the moon, revered as the Lady of the Moon.",
     },
     {
       word: "Suzaku",
-      hint: "A primal associated with fire, known as the Crimson Lady.",
     },
     {
       word: "Seiryu",
-      hint: "A primal associated with water, revered as the Azure Serpent.",
     },
     {
       word: "Titania",
-      hint: "A primal associated with nature, revered as the Fairy Queen.",
     },
   ],
   spellWords: [
-    { word: "Fire", hint: "A basic fire spell, often used to ignite enemies." },
+    { word: "Fire" },
     {
       word: "Blizzard",
-      hint: "A basic ice spell, capable of freezing foes in their tracks.",
     },
     {
       word: "Thunder",
-      hint: "A basic lightning spell, striking foes with bolts of electricity.",
     },
     {
       word: "Aero",
-      hint: "A basic wind spell, creating whirlwinds to buffet enemies.",
     },
     {
       word: "Stone",
-      hint: "A basic earth spell, summoning rocks to crush adversaries.",
     },
     {
       word: "Water",
-      hint: "A basic water spell, unleashing torrents to drown foes.",
     },
     {
       word: "Fira",
-      hint: "An enhanced fire spell, engulfing enemies in a searing inferno.",
     },
     {
       word: "Blizzara",
-      hint: "An enhanced ice spell, encasing enemies in frosty prisons.",
     },
     {
       word: "Thundara",
-      hint: "An enhanced lightning spell, electrifying the battlefield with powerful arcs.",
     },
     {
       word: "Aerora",
-      hint: "An enhanced wind spell, conjuring tempests to sweep away adversaries.",
     },
     {
       word: "Stonega",
-      hint: "An enhanced earth spell, causing earthquakes to shake the ground and topple foes.",
     },
     {
       word: "Watera",
-      hint: "An enhanced water spell, drowning enemies in massive waves.",
     },
     {
       word: "Firaga",
-      hint: "An advanced fire spell, engulfing entire battlefields in raging flames.",
     },
     {
       word: "Blizzaga",
-      hint: "An advanced ice spell, turning entire landscapes into frozen wastelands.",
     },
     {
       word: "Thundaga",
-      hint: "An advanced lightning spell, unleashing devastating bolts that can split mountains.",
     },
     {
       word: "Aeroga",
-      hint: "An advanced wind spell, summoning hurricanes to obliterate anything in their path.",
     },
     {
       word: "Quake",
-      hint: "An advanced earth spell, causing the earth to tremble and swallow foes whole.",
     },
     {
       word: "Tornado",
-      hint: "An advanced wind spell, creating monstrous tornadoes to tear apart enemies.",
     },
     {
       word: "Meteor",
-      hint: "An ultimate spell, summoning meteors from the heavens to annihilate all opposition.",
     },
     {
       word: "Ultima",
-      hint: "An ultimate spell, unleashing the ultimate power to decimate enemies.",
     },
     {
       word: "Cure",
-      hint: "A basic healing spell, restoring health to wounded allies.",
     },
     {
       word: "Curaga",
-      hint: "An enhanced healing spell, capable of fully restoring the the health of multiple allies.",
     },
     {
       word: "Protect",
-      hint: "A protective spell, bolstering the defenses of allies against physical attacks.",
     },
     {
       word: "Shell",
-      hint: "A protective spell, strengthening the magical defenses of allies against spells.",
     },
     {
       word: "Reflect",
-      hint: "A defensive spell, bouncing magical attacks back at their caster.",
     },
     {
       word: "Regen",
-      hint: "A healing spell, gradually restoring the health of allies over time.",
     },
     {
       word: "Haste",
-      hint: "A supportive spell, enhancing the speed and agility of allies.",
     },
     {
       word: "Slow",
-      hint: "A debilitating spell, slowing the movements and actions of enemies.",
     },
     {
       word: "Stop",
-      hint: "An immobilizing spell, freezing enemies in time temporarily.",
     },
     {
       word: "Bio",
-      hint: "A toxic spell, inflicting gradual damage over time to enemies.",
     },
   ],
+
   gameMechanics: [
     {
       word: "Chocobo",
-      hint: "A large, flightless bird used for transportation and combat assistance.",
     },
     {
       word: "Limit Break",
-      hint: "A powerful attack that can turn the tide of battle, shared by the party.",
     },
     {
       word: "Materia",
-      hint: "Magical orbs that enhance abilities when slotted into gear.",
     },
     {
       word: "Glamour",
-      hint: "A system that allows players to change the appearance of their gear.",
     },
     {
       word: "Mount",
-      hint: "Various creatures or vehicles players can ride for faster travel.",
     },
+
     {
       word: "Duty Finder",
-      hint: "A matchmaking system for finding groups to tackle dungeons, trials, and raids.",
     },
     {
       word: "Teleport",
-      hint: "A fast travel method that allows players to instantly move between locations.",
     },
     {
       word: "Aetherial Manipulation",
-      hint: "A teleportation ability used by certain classes in combat.",
     },
     {
       word: "Triple Triad",
-      hint: "A popular card game that players can play against NPCs and other players.",
     },
   ],
 };
@@ -494,6 +389,7 @@ const guessedLettersElement = document.querySelector(".guessed-letters");
 let word;
 let remainingGuesses = 10; // Initialize remaining guesses
 let guessedLetters = []; // Initialize guessed letters array
+let hintUsed = false; // Initialize hint usage flag
 
 const playBackgroundMusic = function () {
   const backgroundMusic = document.getElementById("backgroundMusic");
@@ -545,17 +441,21 @@ const makeGuess = function (guess) {
     guessInput.value = ""; // Clear the input box
   } else {
     guessedLetters.push(guess);
-    updateGuessesRemaining(guess);
+    if (!word.toUpperCase().includes(guess)) {
+      updateGuessesRemaining(guess);
+    }
     showGuessedLetters();
     updateWordInProgress(guessedLetters);
     guessInput.value = ""; // Clear the input box
   }
 };
 
-
 const showGuessedLetters = function () {
   guessedLettersElement.innerHTML = "";
-  for (const letter of guessedLetters) {
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !word.toUpperCase().includes(letter)
+  );
+  for (const letter of incorrectLetters) {
     const li = document.createElement("li");
     li.innerText = letter;
     guessedLettersElement.append(li);
@@ -648,6 +548,8 @@ playAgainButton.addEventListener("click", function () {
   playAgainButton.classList.add("hide");
   remainingGuessesSpan.classList.remove("hide");
   guessedLettersElement.classList.remove("hide");
+  hintUsed = false; // Reset hint usage
+  hintButton.disabled = false; // Enable hint button
 });
 
 const startOver = function () {
@@ -666,14 +568,23 @@ guessButton.addEventListener("click", function (event) {
 });
 
 hintButton.addEventListener("click", function () {
-  const hint = wordData[word].hint;
-  message.innerHTML = `Hint: ${hint}`;
+  if (!hintUsed) {
+    const wordArray = word.toUpperCase().split("");
+    const remainingLetters = wordArray.filter(
+      (letter) => !guessedLetters.includes(letter) && letter !== " "
+    );
 
-  // Show the hint message for 6 seconds
-  const hintTimeout = 6000; // 6 seconds
-  setTimeout(function () {
-    message.innerHTML = ""; // Clear the hint message
-  }, hintTimeout);
+    if (remainingLetters.length > 0) {
+      const randomIndex = Math.floor(Math.random() * remainingLetters.length);
+      const hintLetter = remainingLetters[randomIndex];
+
+      guessedLetters.push(hintLetter);
+      updateWordInProgress(guessedLetters);
+
+      hintUsed = true;
+      hintButton.disabled = true; // Disable the hint button after use
+    }
+  }
 });
 
 // Call initialization function
